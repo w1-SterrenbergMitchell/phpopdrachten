1e10bfc3115eb94a885f056db35d9856eff8676b
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-02-04 12:10:00 +0100

    test 1

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php print "pagina 1";?></title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "hello world";
+                ?>
+        </h1>
+    </body>
+</html>
