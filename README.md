1cb37fd00fd25b24a024d334e8bf9566a893cb57
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-02-05 15:16:31 +0100

    test 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# phpopdrachten
-opdrachten php
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8" />
+    <title><?php print "Index"; ?></title>
+    <link href="CSS/index.css" type="text/css" rel="stylesheet">
+    </head>
+    <div id="header">
+        <h1>Inlever Opdrachten PHP</h1>
+    </div>
+    <div id="menu">
+        <h1>Dit zijn mijn Opdrachten:</h1>
+        <ul>
+            <li>
+                <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+            </li>
+        </ul>
+    </div>
+</html>
