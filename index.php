bbf1e02fcf8a13e75d608d6539824f4a01276545
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-02-10 11:13:03 +0100

    test 3

--- index.php
+++ index.php
@@ -1,19 +1,99 @@
+<!-- /**
+* User: Teun van Helvoort
+* Date: 5/2/2020
+* Time: 2:53 PM
+* File: index.php
+-->
 <!DOCTYPE html>
+
 <html lang="en">
-    <head>
+<head>
     <meta charset="utf-8" />
     <title><?php print "Index"; ?></title>
     <link href="CSS/index.css" type="text/css" rel="stylesheet">
-    </head>
-    <div id="header">
-        <h1>InleverOpdrachten PHP</h1>
-    </div>
-    <div id="menu">
-        <h1>Dit zijn mijn Opdrachten:</h1>
-        <ul>
-            <li>
-                <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
-            </li>
-        </ul>
-    </div>
+</head>
+<body>
+<div id="header">
+    <h1>Inlever Opdrachten PHP</h1>
+</div>
+<div id="menu">
+    <h1>Dit zijn mijn Opdrachten:</h1>
+    <ul>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Opdrachten/Opdracht 1/helloworld.php">helloworld.php</a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+</div>
+<p>Uitwerking</p>
+</body>
 </html>
