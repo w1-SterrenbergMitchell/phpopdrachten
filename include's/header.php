cdaafaf9ab72b874658b8db8a555caad0ae347e8
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-02-18 11:13:15 +0100

    test

---
+++ include's/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "Index"; ?></title>
+    <link href="/Style/index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div id="header">
+    <h1 id="h1">Inleveropdrachten PHP</h1>
+</div>
+</body>
--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <link href="Style/index.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<div id="header">
-    <h1 id="h1">Inleveropdrachten PHP</h1>
-</div>
+<?php
+    include "/inetpub/wwwroot/phpopdrachten/include's/header.php";
+?>
 <div id="menu">
-    <h1>Dit zijn mijn Opdrachten:</h1>
+    <h1>Opdrachten:</h1>
     <ul>
         <li>
             Hoofdstuk 2
