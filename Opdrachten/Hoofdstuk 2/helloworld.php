6e0da2c1faf729d0d83078bf39bb83d4ec0e5390
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-02-18 11:52:18 +0100

    test

--- Opdrachten/Hoofdstuk 2/helloworld.php
+++ Opdrachten/Hoofdstuk 2/helloworld.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <title><?php print "Index"; ?></title>
+    <link href="../../Style/index.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -11,5 +12,7 @@
     include "/inetpub/wwwroot/phpopdrachten/includes/menu.php";
     echo "hello world";
 ?>
+<br>
+<a href="../../index.php">terug</a>
 </body>
 </html>
