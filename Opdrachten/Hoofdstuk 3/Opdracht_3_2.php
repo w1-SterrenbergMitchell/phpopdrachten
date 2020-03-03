8d65dc5c049642e42fc4e8c461bd2b61ef69b1a1
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-03-03 20:40:18 +0100

    opdracht 3.2 af

--- Opdrachten/Hoofdstuk 3/Opdracht_3_2.php
+++ Opdrachten/Hoofdstuk 3/Opdracht_3_2.php
@@ -15,32 +15,46 @@ $trafficlightcolor = "green";
 $ambulancecoming = false;
 $driveon = true;
 
-if($trafficlightcolor = "groen")
+if($trafficlightcolor == "green")
     {
         $driveon = true;
     }
-elseif ($trafficlightcolor = "orange" || $trafficlightcolor = "red")
+elseif ($trafficlightcolor == "orange" || $trafficlightcolor == "red")
     {
         $driveon = false;
     }
-else($ambulancecoming = true)
+else($ambulancecoming == true)
     {
         $driveon = false
     };
 
-if($driveon = true)
+if($driveon == true)
     {
-        echo ("u mag doorrijden.");
-    }
-elseif($driveon = false)
-    {
-        echo ("u moet stoppen.");
+        echo ("<p>"."u mag doorrijden"."</p>"."<br>");
     }
 else
     {
-        echo("ERROR");
+        echo ("<p>"."u moet stoppen"."</p>"."<br>");
     }
-?>
+
+$countryname = "nederland";
+$currentage = 19;
+
+echo ("<p>"."je woont in ".$countryname." en je bent ".$currentage." jaar oud"."</p>"."<br>");
+
+if($currentage <= 15)
+    {
+        echo ("<p>"."je mag hier niets drinken"."</p>");
+    }
+elseif ($currentage == 16 && $countryname == "belgie" || $currentage == 18 && $countryname == "zweden")
+    {
+        echo ("<p>"."je mag hier zwakke alcohol drinken"."</p>");
+    }
+elseif ($currentage >= 17 && $countryname == "cyprus" || $currentage >=18 && $countryname== "belgie" || $currentage >= 18 && $countryname == "bulgarije" || $currentage >= 18 && $countryname == "nederland" || $currentage >= 20 && $countryname == "zweden")
+    {
+        echo ("<p>"."je mag hier sterke alcohol drinken"."</p>");
+    }
+    ?>
 <br>
 <a href="../../index.php">terug</a>
 </body>
