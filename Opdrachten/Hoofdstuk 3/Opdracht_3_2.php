59bffe455c13ac3ba531f5dcaa5db9716ca6efca
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-03-03 11:55:45 +0100

    test

---
+++ Opdrachten/Hoofdstuk 3/Opdracht_3_2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "Index"; ?></title>
+    <link href="/../phpopdrachten/Style/index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<?php
+include "/inetpub/wwwroot/phpopdrachten/includes/header.php";
+include "/inetpub/wwwroot/phpopdrachten/includes/menu.php";
+
+$trafficlightcolor = "green";
+$ambulancecoming = false;
+$driveon = true;
+
+if($trafficlightcolor = "groen")
+    {
+        $driveon = true;
+    }
+elseif ($trafficlightcolor = "orange" || $trafficlightcolor = "red")
+    {
+        $driveon = false;
+    }
+else($ambulancecoming = true)
+    {
+        $driveon = false
+    };
+
+if($driveon = true)
+    {
+        echo ("u mag doorrijden.");
+    }
+elseif($driveon = false)
+    {
+        echo ("u moet stoppen.");
+    }
+else
+    {
+        echo("ERROR");
+    }
+?>
+<br>
+<a href="../../index.php">terug</a>
+</body>
+</html>
