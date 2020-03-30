6a34692e0d9f0c1afdb01de9d4d74c9b116af6f7
mitchell sterrenberg <mitchell.sterrenberg@edu-kw1c.nl>
2020-03-30 11:22:37 +0200

    5.3 toevoegen

---
+++ Opdrachten/Hoofdstuk 5/Les opdracht/lesopdracht_form_data53.php
@@ -0,0 +1,25 @@
+<?php
+//Haal de formulier gegevens op
+//include hieronder "lesopdracht_function.php" die de naam van de docent terug geeft.
+include ""
+?>
+<table>
+    <tr>
+        <td>Voornaam</td>
+        <td>*Toon hier de ingevulde naam*</td>
+    </tr>
+    <!-- Begin: Toon dit alleen als er een tussenvoegsel is ingevuld! -->
+    <tr>
+        <td>Tussenvoegsel</td>
+        <td>*Toon hier tussenvoegsel als deze bestaat *</td>
+    </tr>
+    <!-- Einde: Toon dit alleen als er een tussenvoegsel is ingevuld! -->
+    <tr>
+        <td>Achternaam</td>
+        <td>*Toon hier de ingevulde achternaam*</td>
+    </tr>
+    <tr>
+        <td>Meer informatie</td>
+        <td>*Laad hier de functie die de naam van de juiste docent toont*</td>
+    </tr>
+</table>
---
+++ Opdrachten/Hoofdstuk 5/Les opdracht/lesopdracht_functions53.php
@@ -0,0 +1,10 @@
+<?php
+/*************
+ * Maak hier een functie genaamd "getTeacherName" die als return de juiste docent naam mee geeft.
+ **************/
+
+function getTeacherName(){
+    //Zorg dat je hier de juiste docentnaam met een return terug stuurt.
+    
+}
+?>
